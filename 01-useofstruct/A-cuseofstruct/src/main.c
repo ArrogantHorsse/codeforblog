de6c6af88cd9df0b489622ee177954214c604117
mataojie1 <mataojie1@thundersoft.com>
2021-12-02 14:43:25 +0800

    Time: 2021/12/02 Action: complete the code of struct Owner:mtj

--- 01-useofstruct/A-cuseofstruct/src/main.c
+++ 01-useofstruct/A-cuseofstruct/src/main.c
@@ -2,7 +2,7 @@
 #include "usestruct.h"
 
 int main(){
-    printf("helloWorld!\n");
+    printf("Hello struct of C !\n");
 
     // type 1,在C中定义必须加struct
     struct type_one_s type_one1 = {1,2};
@@ -32,5 +32,19 @@ int main(){
     printf("type 3 : type_three2.a = %d ,type_three2.b = %d\n",type_three2.a,type_three2.b);
     printf("=======================================================\n");
 
-    //type4,
+    //type4, 在定义结构体时添加typedef修饰为结构体取别名
+    struct type_four_s type_four1 = {13,14};      //使用结构体名定义结构体变量,需要加struct
+    type_four_alias_s type_four2 = {15,16};      //使用结构体别名定义结构体变量,不需要加struct
+    printf("type 4 : type_four1.a = %d ,type_four1.b = %d\n",type_four1.a,type_four1.b);
+    printf("type 4 : type_four2.a = %d ,type_four2.b = %d\n",type_four2.a,type_four2.b);
+    printf("=======================================================\n");
+
+    // type5, 在定义结构体时省略结构体名，只取别名
+    type_five_alias_s type_five1 = {17,18};
+    type_five_alias_s type_five2;
+    type_five2.a = 19;
+    type_five2.b = 20; 
+    printf("type 5 : type_five1.a = %d ,type_five1.b = %d\n",type_five1.a,type_five1.b);
+    printf("type 5 : type_five2.a = %d ,type_five2.b = %d\n",type_five2.a,type_five2.b);
+    printf("=======================================================\n");
 }
--- 01-useofstruct/B-cppuseofstruct/src/main.cpp
+++ 01-useofstruct/B-cppuseofstruct/src/main.cpp
@@ -1,13 +1,56 @@
 #include<iostream>
 #include "usestruct.h"
-using namespace std;
+
+using std::cout;
+using std::endl;
 
 int main(){
-    #ifdef __cplusplus
-    extern "C" {
-    #endif
-    
-    #ifdef __cplusplus
-    }
-    #endif
+    cout<<"hello struct of C++ !"<<endl;
+
+    // type one,在C++ 中定义不需加struct
+    // 结构体赋值方法一，使用花括号
+    type_one_s type_one1 = {1,2};  
+
+    // 结构体赋值方法二，直接赋值
+    type_one_s type_one2;
+    type_one2.a = 3;
+    type_one2.b = 4;
+
+    cout<<"type one : type_one1.a ="<<type_one1.a<<"  type_one1.b ="<<type_one1.b<<endl;
+    cout<<"type one : type_one2.a ="<<type_one2.a<<"  type_one2.b ="<<type_one2.b<<endl;
+    cout<<"======================================================="<<endl;
+
+    // type two,与type one 用法一致，但可直接使用在定义结构体时定义的结构体变量
+    type_two1.a = 5;
+    type_two1.b = 6;
+
+    type_two_s type_two2 = {7,8};
+    cout<<"type two : type_two1.a ="<<type_two1.a<<"  type_two1.b ="<<type_two1.b<<endl;
+    cout<<"type two : type_two2.a ="<<type_two2.a<<"  type_two2.b ="<<type_two2.b<<endl;
+    cout<<"======================================================="<<endl;
+
+    // type3, 在定义结构体时无结构体名称，只能使用已定义的结构体变量
+    type_three1.a = 9;
+    type_three1.b = 10;
+    type_three2.a = 11;
+    type_three2.b = 12;
+    cout<<"type three : type_three1.a ="<<type_three1.a<<"  type_three1.b ="<<type_three1.b<<endl;
+    cout<<"type three : type_three2.a ="<<type_three2.a<<"  type_three2.b ="<<type_three2.b<<endl;
+    cout<<"======================================================="<<endl;
+
+    //type4, 在定义结构体时添加typedef修饰,为结构体取别名
+    type_four_s type_four1 = {13,14};            //使用结构体名定义结构体变量,不需要加struct
+    type_four_alias_s type_four2 = {15,16};      //使用结构体别名定义结构体变量,不需要加struct
+    cout<<"type four : type_four1.a ="<<type_four1.a<<"  type_four1.b ="<<type_four1.b<<endl;
+    cout<<"type four : type_four2.a ="<<type_four2.a<<"  type_four2.b ="<<type_four2.b<<endl;
+    cout<<"======================================================="<<endl;
+
+    // type5, 在定义结构体时省略结构体名，只取别名
+    type_five_alias_s type_five1 = {17,18};
+    type_five_alias_s type_five2;
+    type_five2.a = 19;
+    type_five2.b = 20; 
+    cout<<"type five : type_five1.a ="<<type_five1.a<<"  type_five1.b ="<<type_five1.b<<endl;
+    cout<<"type five : type_five2.a ="<<type_five2.a<<"  type_five2.b ="<<type_five2.b<<endl;
+    cout<<"======================================================="<<endl;
 }
